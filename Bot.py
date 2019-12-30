0b9806892f0ffd595cbad5f2d823b639e58b3535
Mikhaylov-yv <dezmond22@yandex.ru>
2019-12-30 20:20:06 +0300

    Cmmit 4

--- Bot.py
+++ Bot.py
@@ -4,8 +4,19 @@ import bot_config
 
 bot = telebot.TeleBot(bot_config.token)
 
+def message_generait(text):
+    if text == 'привет':
+        return 'Привет, мой создатель'
+    if text == 'пока':
+        return 'Прощай, создатель'
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+        bot.send_message(message.chat.id, message_generait(message.text.lower()))
+
 
 bot.polling()
